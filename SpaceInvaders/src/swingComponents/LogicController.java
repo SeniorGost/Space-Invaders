9de06b8657b1a1985416f38fc96d079f7c0224f7
SeniorGost <gorkah25@gmail.com>
2026-02-17 22:38:38 +0100

    Placeholder
    Un placeholder para poder sincronizar un proyecto

---
+++ SpaceInvaders/src/swingComponents/LogicController.java
@@ -0,0 +1,5 @@
+package swingComponents;
+
+public final class LogicController {
+
+}
---
+++ SpaceInvaders/src/swingComponents/MyGame.java
@@ -0,0 +1,10 @@
+package swingComponents;
+
+public class MyGame {
+
+	public static void main(String[] args) {
+		GameFrame myFrame = new GameFrame();
+		myFrame.setVisible(true);
+	}
+
+}
