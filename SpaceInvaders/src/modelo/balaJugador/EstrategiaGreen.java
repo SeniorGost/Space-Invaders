451b9027094bca8c6bc272d06d7b05fe25649661
Pablo Fernández González <166015266+githubdetrabajo1234456@users.noreply.github.com>
2026-04-14 09:00:54 +0200

    Desarrollando el plan S.H.R.E.K
    Lo mismo que el commit anterior pero para la estrategia de disparo de la nave verde (El 'cherk car')

--- SpaceInvaders/src/modelo/balaJugador/EstrategiaGreen.java
+++ SpaceInvaders/src/modelo/balaJugador/EstrategiaGreen.java
@@ -3,8 +3,7 @@ package modelo.balaJugador;
 public class EstrategiaGreen implements EstrategiaDisparo {
     @Override
     public String elegirTipoBala(String inputVista) {
-        // Suponiendo que el input es "Siguiente" o un nombre específico
-        if (inputVista.equalsIgnoreCase("Flecha")) return "Flecha";
+        if (inputVista.equalsIgnoreCase("Pixel")) return "Flecha";
         return "Pixel"; 
     }
 }
