4d29b59fd28a0cb3a49cd4546a02f8c54ace80a7
Pablo Fernández González <166015266+githubdetrabajo1234456@users.noreply.github.com>
2026-04-14 08:58:14 +0200

    Reelaborando estrategias azules
    Ajustada la clase para que se cambie correctamente a lo que se tiene que disparar cada vez que se da al boton de cambiar de estrategia

--- SpaceInvaders/src/modelo/balaJugador/EstrategiaBlue.java
+++ SpaceInvaders/src/modelo/balaJugador/EstrategiaBlue.java
@@ -3,8 +3,7 @@ package modelo.balaJugador;
 public class EstrategiaBlue implements EstrategiaDisparo {
     @Override
     public String elegirTipoBala(String inputVista) {
-    	// Suponiendo que el input es "Siguiente" o un nombre específico
-        if (inputVista.equalsIgnoreCase("Rombo")) return "Rombo";
+        if (inputVista.equalsIgnoreCase("Pixel")) return "Rombo";
         return "Pixel";
     }
 }
